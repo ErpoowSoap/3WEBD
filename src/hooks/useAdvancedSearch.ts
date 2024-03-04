1520916ae7da7b25b9e3a66a619c8313c0260f74
ErpoowSoap <66976703+ErpoowSoap@users.noreply.github.com>
2024-03-04 16:52:20 +0100

    Add files via upload

---
+++ src/hooks/useAdvancedSearch.ts
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { TestBook } from '../types';
+
+
+const useAdvancedSearch = () => {
+    const [searchCriteria, setSearchCriteria] = useState({
+        title: '',
+        author: '',
+        publishYear: '',
+        isbn: '',
+        theme: '',
+        publisher: ''
+    });
+    const [results, setResults] = useState<TestBook[]>([]);
+
+    const handleSearch = async () => {
+        try {
+            const url = new URL('https://openlibrary.org/search.json');
+
+            Object.entries(searchCriteria).forEach(([key, value]) => {
+                if (value && key in searchCriteria) {
+                    url.searchParams.append(key, value);
+                }
+            });
+
+            const response = await fetch(url.toString());
+            const data = await response.json();
+            console.log(data);
+            setResults(data.docs);
+        } catch (error) {
+            console.error('Error fetching search results:', error);
+        }
+    };
+
+    return {
+        searchCriteria,
+        setSearchCriteria,
+        results,
+        handleSearch
+    };
+};
+
+export default useAdvancedSearch;
