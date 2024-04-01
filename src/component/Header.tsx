f3e0821a722cec1aa055a9a220b447234dc27d9a
Paul AF <66976703+ErpoowSoap@users.noreply.github.com>
2024-04-02 01:17:58 +0200

    Search optimisé

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { Autocomplete, Group, Burger, rem } from "@mantine/core";
+import { Autocomplete, Group, Burger, rem, Button } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconSearch } from "@tabler/icons-react";
 import classes from "./Header.module.css";
@@ -7,77 +7,79 @@ import useGeneralSearch from "../hooks/useGeneralSearch.ts";
 import { AdvancedSearch } from "../types.ts";
 
 const links = [
-  { link: "/playlist", label: "ReadList" },
-  { link: "/search", label: "Advanced Search" },
+    { link: "/playlist", label: "ReadList" },
+    { link: "/search", label: "Advanced Search" },
 ];
 
 export function Header() {
-  const [opened, { toggle }] = useDisclosure(false);
-  const { query, setQuery, results, handleSearch } = useGeneralSearch();
-  const navigate = useNavigate();
+    const [opened, { toggle }] = useDisclosure(false);
+    const { query, setQuery, results, handleSearch } = useGeneralSearch();
+    const navigate = useNavigate();
 
-  const items = links.map((link) => (
-    <Link key={link.label} to={link.link} className={classes.link}>
-      {link.label}
-    </Link>
-  ));
+    const handleSearchClick = () => {
+        handleSearch();
+    };
 
-  return (
+    const items = links.map((link) => (
+        <Link key={link.label} to={link.link} className={classes.link}>
+            {link.label}
+        </Link>
+    ));
 
-      <header className={classes.header}>
-          <div className={classes.inner}>
-              <Group>
-                  <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm"/>
-                  <Link to="/">
-                      <img
-                          className={classes.logo}
-                          src="./src/assets/image/logo.png"
-                          alt="/"
-                      />
-                  </Link>
-                  <h3>Sagesse Eternelle</h3>
-              </Group>
+    return (
+        <header className={classes.header}>
+            <div className={classes.inner}>
+                <Group>
+                    <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+                    <Link to="/">
+                        <img
+                            className={classes.logo}
+                            src="./src/assets/image/logo.png"
+                            alt="/"
+                        />
+                    </Link>
+                    <h3>Sagesse Eternelle</h3>
+                </Group>
 
-              <Group>
-                  <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
-                      {items}
-                  </Group>
-                  <Autocomplete
-                      className={classes.search}
-                      placeholder="Search"
-                      leftSection={
-                          <IconSearch
-                              style={{width: rem(16), height: rem(16)}}
-                              stroke={1.5}
-                          />
-                      }
-                      data={results.map((result: AdvancedSearch) => result.title).filter((value, index, self) => self.indexOf(value) === index)}
-                      value={query}
-                      onChange={(value) => {
-                          setQuery(value);
-                          handleSearch();
-                      }}
-                      visibleFrom="xs"
-                      onSelect={(event) => {
-                          const selectedOption = (event.target as HTMLInputElement).value;
-                          const selectedBook = results.find(result => result.title === selectedOption);
-                          if (selectedBook) {
-                              const keyParts = selectedBook.key.split('/');
-                              const bookId = keyParts[keyParts.length - 1];
-                              navigate(`/details/${bookId}`);
-                          }
-
-                      }}
-                  />
-              </Group>
-          </div>
-          <div>
-              {opened && (
-                  <Group className={classes.links}>
-                      {items}
-                  </Group>
-              )}
-          </div>
-      </header>
-  );
+                <Group>
+                    <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
+                        {items}
+                    </Group>
+                    <Autocomplete
+                        className={classes.search}
+                        placeholder="Search"
+                        leftSection={
+                            <IconSearch
+                                style={{ width: rem(16), height: rem(16) }}
+                                stroke={1.5}
+                            />
+                        }
+                        data={results.map((result: AdvancedSearch) => result.title).filter((value, index, self) => self.indexOf(value) === index)}
+                        value={query}
+                        onChange={(value) => {
+                            setQuery(value);
+                        }}
+                        visibleFrom="xs"
+                        onSelect={(event) => {
+                            const selectedOption = (event.target as HTMLInputElement).value;
+                            const selectedBook = results.find(result => result.title === selectedOption);
+                            if (selectedBook) {
+                                const keyParts = selectedBook.key.split('/');
+                                const bookId = keyParts[keyParts.length - 1];
+                                navigate(`/details/${bookId}`);
+                            }
+                        }}
+                    />
+                    <Button onClick={handleSearchClick} variant="outline">Search</Button>
+                </Group>
+            </div>
+            <div>
+                {opened && (
+                    <Group className={classes.links}>
+                        {items}
+                    </Group>
+                )}
+            </div>
+        </header>
+    );
 }
